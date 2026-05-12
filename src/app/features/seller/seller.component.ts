1436b7a55184425cc13b0c5534290883ea7c0430
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2026-05-12 16:05:39 -0500

    Seller panel

--- src/app/features/seller/seller.component.ts
+++ src/app/features/seller/seller.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-seller',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './seller.component.html',
   styleUrl: './seller.component.scss',
 })
